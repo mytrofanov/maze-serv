e0a35400c6067345c34e856ef64c4700ccc76bda
mytrofanov <mytrofanovmaxym@gmail.com>
2023-10-24 10:14:36 +0300

    feat: without PG only sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import * as path from 'path';
 import { SequelizeModule } from '@nestjs/sequelize';
-import * as process from 'process';
+//import * as process from 'process';
 import { GameModule } from './game/game.module';
 
 @Module({
@@ -15,12 +15,16 @@ import { GameModule } from './game/game.module';
             rootPath: path.resolve(__dirname, 'static'),
         }),
         SequelizeModule.forRoot({
-            dialect: 'postgres',
-            host: process.env.POSTGRES_HOST,
-            port: Number(process.env.POSTGRES_PORT),
-            username: process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD,
-            database: process.env.POSTGRES_DP,
+            //TO USE sqlite:
+            dialect: 'sqlite',
+            storage: './database.sqlite',
+            //TO USE PG-ADMIN:
+            // dialect: 'postgres',
+            // host: process.env.POSTGRES_HOST,
+            // port: Number(process.env.POSTGRES_PORT),
+            // username: process.env.POSTGRES_USER,
+            // password: process.env.POSTGRES_PASSWORD,
+            // database: process.env.POSTGRES_DP,
             models: [],
             autoLoadModels: true,
         }),
